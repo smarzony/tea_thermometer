eaf32616f75a09479c423281366fa2a2dc9654c0
Piotr Smarzynski <smarzony@gmail.com>
2021-08-23 20:52:53 +0200

    working DS18B20, not working SSD oled

--- tea_thermometer.ino
+++ tea_thermometer.ino
@@ -1,8 +1,29 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <OneWire.h>
 #include <DallasTemperature.h>
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+#define OLED_RESET     -4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup()
 {
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) 
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+    }
+    // display.clearDisplay();
+    sensors.begin();
     pinMode(13, OUTPUT);
     Serial.begin(9600);
     Serial.println("Oj tak byczq");
@@ -11,9 +32,24 @@ void setup()
 void loop()
 
 {
-    Serial.println("+1");
-    digitalWrite(13, LOW);
+
+  sensors.requestTemperatures(); 
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Check if reading was successful
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+
+    display.setCursor(0,0);
+    display.println(String(tempC, 1));
+    display.display();
     delay(500);
-    digitalWrite(13, HIGH);
-    delay(500);
+    display.clearDisplay();
+  } 
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
 }
